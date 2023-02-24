dd21415fe558748202aa52ecbee410fb69a119a6
benjamin papp <benjaminpapp@Benjamins-MacBook-Pro-14.local>
2023-02-24 23:24:19 +0100

    Remove comment.

--- src/store/redux.ts
+++ src/store/redux.ts
@@ -17,7 +17,6 @@ const store = configureStore({
 /** Typescript support. */
 // Infer the `RootState` and `AppDispatch` types from the store itself
 type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 type AppDispatch = typeof store.dispatch;
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
